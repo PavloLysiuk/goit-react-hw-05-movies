d428d8b2cc627bbdc8a1add9bd7aeec9f0827a85
PavloLysiuk <pavel.lysiuk@gmail.com>
2023-09-23 18:23:23 +0000

    Deploying to gh-pages from @ PavloLysiuk/goit-react-hw-05-movies@cb88aae55151eb5856c9ed1d1c45c4e483063961 🚀

--- static/js/304.f4f6532e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[304],{304:function(e,n,t){t.r(n),t.d(n,{default:function(){return l}});var r,o,a=t(168),c=t(867),u=c.zo.main(r||(r=(0,a.Z)([""]))),i=c.zo.h1(o||(o=(0,a.Z)(["\n  text-align: center;\n  text-transform: uppercase;\n  font-weight: 800;\n  color: #555555;\n"]))),h=t(184),l=function(){return(0,h.jsx)(u,{children:(0,h.jsx)(i,{children:"Trending today"})})}}}]);
-//# sourceMappingURL=304.f4f6532e.chunk.js.map
