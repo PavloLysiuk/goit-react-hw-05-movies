08e9e4407f6c63ad2005824123156fa2a31c8b5e
Pavlo Lysiuk <121764119+PavloLysiuk@users.noreply.github.com>
2023-09-23 16:27:55 +0300

    add folders and files

--- src/api/api.js
+++ src/api/api.js
@@ -6,18 +6,22 @@ const API_KEY = '296730eb54efbbc44cb6aeed23da7d0a';
 axios.defaults.baseURL = BASE_URL;
 
 export default async function fetchData(searchQuery, param = {}) {
-  const config = {
+  const options = {
     method: 'GET',
     baseURL: BASE_URL,
+    headers: {
+      accept: 'application/json',
+    },
     params: {
       api_key: API_KEY,
       ...param,
     },
   };
 
-  const response = await axios.get(searchQuery, config);
+  const response = await axios.get(searchQuery, options);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
+  console.log(response.data);
   return response.data;
 }
